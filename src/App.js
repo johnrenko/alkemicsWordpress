9c7a8eed7b1bc3470a640a31185aec857b5b1152
John Dutamby <jdutamby@alkemics.com>
2017-07-05 10:23:47 +0200

    Wip

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import HeroHeader from "./Modules/heroHeader";
 import Infoblocks from "./Modules/Infoblocks";
-import Button from "./Components/Button";
 
 class App extends Component {
 	render() {
 		return (
 			<div>
+				<HeroHeader />
 				<Infoblocks />
-				<Button primary content="Customers" />
 			</div>
 		);
 	}
