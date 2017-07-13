06eae8fd7c5f9d9ed4d8b82b50a8a823ea6dd11f
John Dutamby <jdutamby@alkemics.com>
2017-07-13 14:39:26 +0200

    wip

--- src/Views/Homepage/index.js
+++ src/Views/Homepage/index.js
@@ -9,12 +9,40 @@ import BoxContent from '../../Modules/BoxContent';
 import Quote from '../../Components/Quote';
 
 class Homepage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      title: '',
+      subtitle: '',
+      heroImage: '',
+      ctaText: '',
+    };
+  }
+
+  componentDidMount() {
+    let dataURL = 'http://www.techalkemics.odns.fr/wp-json/wp/v2/pages';
+    fetch(dataURL).then(res => res.json()).then(res => {
+      this.setState({
+        title: res[0].acf.title,
+        subtitle: res[0].acf.subtitle,
+        heroImage: res[0].acf.heroimage,
+        ctaText: res[0].acf.cta_top,
+      });
+    });
+  }
+
   render() {
     return (
       <div>
         <div className="NavHeader">
           <NavBar />
-          <HeroHeader />
+          <HeroHeader
+            title={this.state.title}
+            subtitle={this.state.subtitle}
+            heroImage={this.state.heroImage}
+            ctaText={this.state.ctaText}
+            ctaLink=""
+          />
         </div>
         <ValueBlock />
         <BoxContent />
